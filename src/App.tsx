5e4f1d75bc0176f052827c76f7fcacda02dd0abf
Antonija Simic <antonija1023@gmail.com>
2022-06-29 20:32:26 +0200

    rename routes file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Container from './components/Layout/Container';
-import Routes from './routes/routes';
+import Routes from './routes/AppRoutes';
 function App() {
     return (
         <Container>
