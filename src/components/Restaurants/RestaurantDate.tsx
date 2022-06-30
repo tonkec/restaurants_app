2eac949404af8ed3c539db9fd245224eb21ff0c7
Antonija Simic <antonija1023@gmail.com>
2022-06-30 09:50:40 +0200

    check if restaurant is currently open

--- src/components/Restaurants/RestaurantDate.tsx
+++ src/components/Restaurants/RestaurantDate.tsx
@@ -1,16 +1,29 @@
 import moment from 'moment';
-import { Day } from '../../models/Restaurant';
+import { Day, RestaurantModel } from '../../models/Restaurant';
 
-const RestaurantDate = ({ restaurantDate }: any) => {
-    const curerntDate = new Date();
-    const currentDay: Day = moment(curerntDate)
+const RestaurantDate = ({
+    restaurantDate,
+}: {
+    restaurantDate: RestaurantModel['hours'];
+}) => {
+    const currentDate = new Date();
+    const currentDay: Day = moment(currentDate)
         .format('dddd')
         .toLowerCase() as Day;
     const currentHours = restaurantDate[currentDay];
+    const openningHours = currentHours.opens_at;
+    const closingHours = currentHours.closes_at;
+    const currentTime = moment(currentDate).format('LT');
+
+    const isCurrentlyOpen = moment(currentTime, 'LT').isBetween(
+        moment(openningHours, 'LT'),
+        moment(closingHours, 'LT')
+    );
+
     return (
         <p>
             {currentHours.opens_at} - {currentHours.closes_at} -{' '}
-            {currentHours.is_closed ? 'closed' : 'open'}
+            {isCurrentlyOpen ? 'open' : 'closed'}
         </p>
     );
 };
