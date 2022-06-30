238f1537a8d5e555e33cca318be594955383f5cc
Antonija Simic <antonija1023@gmail.com>
2022-06-30 09:50:48 +0200

    fix paddings

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -1,13 +1,13 @@
 import Restaurants from '../Restaurants/Restaurants';
-import Sidebar from './../Sidebar/Sidebar';
+import Sidebar from '../Sidebar/Sidebar';
 const Homepage = () => (
-    <>
+    <div className="py-24">
         <h1 className="text-5xl font-bold mb-6">
             Welcome to the Restaurants app!
         </h1>
         <Sidebar />
         <Restaurants />
-    </>
+    </div>
 );
 
 export default Homepage;
